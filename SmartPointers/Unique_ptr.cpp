00757009623c1cfb24612b6dcacb00fcd1515a6e
Palash Jain <palashjain867@gmail.com>
2025-03-10 17:35:09 +0000

    Initial Class for the unique pointer

---
+++ SmartPointers/Unique_ptr.cpp
@@ -0,0 +1,14 @@
+// Implement unique_ptr (Exclusive Ownership)
+// 	• Define a template class UniquePtr<T>.
+// 	• Use a raw pointer (T* ptr) to store the managed object.
+// 	• Implement: 
+// 		○ Constructor: Accepts a raw pointer.
+// 		○ Destructor: Deletes the managed object.
+// 		○ Move semantics: Implement move constructor and move assignment operator.
+// 		○ Delete copy constructor and copy assignment to enforce unique ownership.
+// Overload operator* and operator-> for object access.
+
+class myUniquePtr{
+    
+
+};
