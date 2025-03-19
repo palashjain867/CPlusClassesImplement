4588bbb71636dd19c7f4658f0ac2605b9583e244
Palash Jain <palashjain867@gmail.com>
2025-03-19 06:57:39 +0000

    mySharedPtr: Correctly handle self-assignment in move assignment operator

--- SmartPointers/Shared_ptr.cpp
+++ SmartPointers/Shared_ptr.cpp
@@ -14,12 +14,91 @@
 template <typename T>
 class mySharedPtr{
     T* ptr;
-    int* refCount {0};
+    int* refCount;
+
+    void decrementRefCount(){
+        if(refCount){
+            (*refCount)--;
+            if(*refCount == 0){
+                delete ptr;
+                delete refCount;
+
+                ptr = nullptr;
+                refCount = nullptr;
+            }
+        }
+    }
 
     public:
-        mySharedPtr(T* inputPtr) : ptr(inputPtr)
+        mySharedPtr(T* inputPtr = nullptr)
         {
-            refCount++;
+            if(inputPtr)
+            {
+                ptr = inputPtr;
+                refCount = new int(1);
+            }
+        }
+
+        ~mySharedPtr()
+        {
+            decrementRefCount();
+             
+        }
+        mySharedPtr(const mySharedPtr& other){
+            ptr = other.ptr;
+            refCount = other.refCount;
+
+            if(refCount){
+                (*refCount)++;
+            }
+        }
+
+        mySharedPtr& operator=(const mySharedPtr& other){
+            if(this != other){
+                decrementRefCount();
+                ptr = other.ptr;
+                refCount = other.refCount;
+
+                if(refCount){
+                    (*refCount)++;
+                }
+            }
+            return *this;
+        }
+
+        mySharedPtr(const mySharedPtr && other) : ptr(other.ptr),
+                                                refCount(other.refCount)
+        {
+            other.ptr = nullptr;
+            other.refCount = nullptr;
+        }
+
+        //why using noexcept
+        mySharedPtr& operator=(mySharedPtr&& other){
+            if(this != other){
+                decrementRefCount(); //why we have not used in move constructor
+                ptr = other.ptr;
+                refCount = other.refCount;
+
+                other.ptr = nullptr;
+                other.refCount = nullptr;
+            }
+            return *this;
+        }
+
+        //Deference operator
+        T& operator*() const{
+            if(!ptr){
+                throw std::runtime_error("Deferencing a null pointer");
+            }
+            return *ptr;
+        }
+
+        T* operator->() const{
+            if(!ptr){
+                throw std::runtime_error("Deferencing a null pointer");
+            }
+            return ptr;
         }
 };
 
